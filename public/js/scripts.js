3444446953ba2249908abdd37c806399c33fb6f2
Liz Karol Vergara <cva.k16@gmail.com>
2024-11-03 17:08:33 -0500

    primer commit

---
+++ public/js/scripts.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    iniciarApp();
+});
+
+function iniciarApp() {
+    navegacionFija();
+}
+
+function navegacionFija() {
+    const barra = document.querySelector('header');
+    const body = document.querySelector('body')
+
+    window.addEventListener('scroll', function() {
+        console.log(barra.getBoundingClientRect());
+
+        if (barra.getBoundingClientRect().bottom) {
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    });
+}
